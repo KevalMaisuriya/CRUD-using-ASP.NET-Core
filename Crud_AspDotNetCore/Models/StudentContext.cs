04571e078f6ff2f1357cf1c97891b6efab0cc7ea
Keval Maisuriya <kevalmaisuriyaelsner@gmail.com>
2022-04-16 11:51:34 +0530

    Upload

---
+++ Crud_AspDotNetCore/Models/StudentContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud_AspDotNetCore.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<Student> tbl_Student { get; set; }
+        public DbSet<Departments> tbl_Departments { get; set; }
+
+    }
+}
